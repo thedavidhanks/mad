c69eba263ecd4f7cccc222ebbe2026155f819875
David Hanks <dphanks@gmail.com>
2020-01-05 20:57:50 -0600

    added instapost state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import InstagramEmbed from 'react-instagram-embed';
+
+import PostList from './PostList.js';
 
 class Home extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+          posts: [
+              { "url": "B64n-rcJopb", "date": "1-3-2020", "user" : "magpie88", "key": 1},
+              { "url": "BaJ-5o3By9u", "date": "12-5-2019", "user" : "thedavidhanks", "key": 2}
+          ]
+      };  
+    }
+    
     render() {
         return (
             <Container className="home">
-                <InstagramEmbed
-                    url='https://www.instagram.com/p/B64n-rcJopb/'
-                    maxWidth={800}
-                    hideCaption={false}
-                />
-                <InstagramEmbed
-                    url='https://www.instagram.com/p/BaJ-5o3By9u/'
-                    maxWidth={800}
-                    hideCaption={false}
-                />
+                {this.state.posts !== [] ? <PostList posts={this.state.posts} /> : null}
             </Container>
         );
     }
---
+++ src/components/PostList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
+
+
+const PostList = props =>{
+    //props.markers will be an array of marker objects
+    
+    //cycle through each element of the array.
+    const postlist = props.posts.map((post, index) => {
+        //console.log(marker);
+        
+        //const dateAdded = (marker.addedOn) ? marker.addedOn.toDate() : null ;
+        
+        return(
+            <InstagramEmbed
+                    url={"https://www.instagram.com/p/".concat(post.url,"/")}
+                    maxWidth={800}
+                    hideCaption={false}
+                    key={post.key}
+            />
+        );}
+    );
+    return postlist;
+};
+
+export default PostList;
